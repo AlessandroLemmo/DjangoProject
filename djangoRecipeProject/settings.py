d3d73d4f346f911e4366845365ec813ea8591a44
Alessandro Lemmo <alessandro.lemmo@stud.unifi.it>
2023-06-14 00:10:10 +0200

    Add files via upload

--- djangoRecipeProject/settings.py
+++ djangoRecipeProject/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-from django.contrib import staticfiles
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,22 +122,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/Users/lauralemmo/Desktop/djangoRecipeProject/static/'
 
-
-#STATIC_ROOT = '/Users/lauralemmo/Desktop/djangoRecipeProject/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-]
 
 #STATICFILES_DIRS = [
      #Percorso in cui si trovano i tuoi file statici
 #    os.path.join(BASE_DIR, 'static')
 #]
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
